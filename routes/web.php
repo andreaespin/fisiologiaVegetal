625193cc679afa039f769f6bdefd47aec543fe15
andreaespin <andreaespin9608@gmail.com>
2019-11-11 19:01:49 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::post('post/save', 'PostController@store');
+// Route::post('post/save', 'PostController@store');
 
-Route::get('/','HomeController@index');
+// Route::get('/','HomeController@index');
